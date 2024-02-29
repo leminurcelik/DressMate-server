a0c944362270be17fb75ee082ffb165764d1fef8
hilalerdogan <erdoganh19@itu.edu.tr>
2024-02-29 15:49:17 +0300

    Added options config

---
+++ config/clothingItemOptions.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    colorOptions: ['Red', 'Blue', 'Green', 'Yellow', 'Black', 'White', 'Gray'],
+    styleOptions: ['Casual', 'Formal', 'Sportswear', 'Evening'],
+    categoryOptions: ['Top', 'Bottom', 'Shoes', 'One-piece', 'Outerwear'],
+    subCategoriesofTop: ['T-shirt', 'Blouse', 'Sweater'],
+    subCategoriesofBottom: ['Pants', 'Shorts', 'Jeans'],
+    subCategoriesofShoes: ['Sneakers', 'Sandals', 'Boots'],
+    subCategoriesofOnePiece: ['Dress', 'Jumpsuit'],
+    subCategoriesofOuterwear: ['Jacket', 'Coat'],
+    weatherOptions: ['Hot', 'Cold']
+};
