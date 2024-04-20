242df09eec7c6c9ddf6d1ee0d8c0221b16b12b7b
leminurcelik <celikleminur@gmail.com>
2024-04-20 19:12:56 +0300

    Updated color options

--- config/clothingItemOptions.js
+++ config/clothingItemOptions.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    colorOptions: ['Red', 'Blue', 'Green', 'Yellow', 'Black', 'White', 'Gray', 'Brown', 'Pink','Purple','Orange','Gray','Beige'],
+    colorOptions: ['Red', 'Blue', 'Green', 'Yellow', 'Black', 'White', 'Gray', 'Brown', 'Pink','Purple','Orange','Beige'],
     styleOptions: ['Casual', 'Formal', 'Sportswear', 'Evening'],
     categoryOptions: ['Top', 'Bottom', 'Shoes', 'One-piece', 'Outerwear'],
     subCategoriesofTop: ['T-shirt', 'Blouse', 'Sweater','Shirt'],
